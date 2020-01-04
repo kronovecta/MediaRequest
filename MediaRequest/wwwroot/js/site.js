07b2216a038d3a15da6f4c77ac71a081ba8e3158
Robert Nordgren <robert.b.nordgren@gmail.com>
2020-01-04 23:49:28 +0100

    - Updated responsive and mobile layout of startpage

--- MediaRequest/wwwroot/js/site.js
+++ MediaRequest/wwwroot/js/site.js
@@ -1,4 +1,18 @@
-﻿let mobilenav = $('.mobilenav');
+﻿function scrollToAnchor(anchor) {
+    let aTag = $("#" + anchor);
+    let navHeight = 0;
+    $('.navbar').outerHeight(false) == 0 ? navHeight = $('.mobile-top-nav').outerHeight(false) : navHeight = $('.navbar').outerHeight(false);
+    $('html,body').animate({ scrollTop: (aTag.offset().top - navHeight) }, 'slow');
+}
+
+// INDEX
+$(document).on('click', '.movie-list-container > .pagination > a', function () {
+    scrollToAnchor('movies-partial-container');
+    $('.loading').show();
+})
+
+// MOBILE NAV
+let mobilenav = $('.mobilenav');
 let mobilemenu = $('.mobilenav > .nav-item-container');
 let mobilebackdrop = $('.mobilenav > .backdrop');
 let mobileMenuWidth = mobilemenu.outerWidth(true);
@@ -12,8 +26,6 @@ $(mobileMenuIcon).on('click', function () {
     mobileMenuShow();
 })
 
-//$(mobileMenuIcon).on('click', mobileMenuShow());
-
 function mobileMenuShow() {
     $(mobilenav).show();
     $(mobilemenu).animate({ 'left': '0' }, 'fast');
@@ -24,6 +36,6 @@ function mobileMenuHide() {
     $(mobilemenu).animate({ 'left': '-300px' }, 'fast', function () {
         $(mobilebackdrop).fadeOut('fast')
         $(mobilenav).hide();
-    });
-    
+    });   
 }
+
