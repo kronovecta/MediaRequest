783faad7004c86ed3ce777b8b7844813782163ef
Robert Nordgren <robert.b.nordgren@gmail.com>
2020-11-20 11:13:15 +0100

    Update pixel scaling and mobile navbar adjustements

---
+++ MediaRequest/wwwroot/js/enhancers/mobile_nav.js
@@ -0,0 +1,27 @@
+﻿// MOBILE NAV
+let mobilenav = $('.mobilenav');
+let mobilemenu = $('.mobilenav > .nav-item-container');
+let mobilebackdrop = $('.mobilenav > .backdrop');
+let mobileMenuWidth = mobilemenu.outerWidth(true);
+let mobileMenuIcon = $('.mobile-menu-icon');
+
+$(mobilebackdrop).on('click', function () {
+    mobileMenuHide();
+})
+
+$(mobileMenuIcon).on('click', function () {
+    mobileMenuShow();
+})
+
+function mobileMenuShow() {
+    $(mobilenav).show();
+    $(mobilemenu).animate({ 'left': '0' }, 'fast');
+    $(mobilebackdrop).show();
+}
+
+function mobileMenuHide() {
+    $(mobilemenu).animate({ 'left': '-300px' }, 'fast', function () {
+        $(mobilebackdrop).fadeOut('fast')
+        $(mobilenav).hide();
+    });
+}
--- MediaRequest/wwwroot/js/site.js
+++ MediaRequest/wwwroot/js/site.js
@@ -11,33 +11,7 @@ $(document).on('click', '.movie-list-container > .pagination > a', function () {
     $('.loading').show();
 })
 
-// MOBILE NAV
-let mobilenav = $('.mobilenav');
-let mobilemenu = $('.mobilenav > .nav-item-container');
-let mobilebackdrop = $('.mobilenav > .backdrop');
-let mobileMenuWidth = mobilemenu.outerWidth(true);
-let mobileMenuIcon = $('.mobile-menu-icon');
 
-$(mobilebackdrop).on('click', function () {
-    mobileMenuHide();
-})
-
-$(mobileMenuIcon).on('click', function () {
-    mobileMenuShow();
-})
-
-function mobileMenuShow() {
-    $(mobilenav).show();
-    $(mobilemenu).animate({ 'left': '0' }, 'fast');
-    $(mobilebackdrop).show();
-}
-
-function mobileMenuHide() {
-    $(mobilemenu).animate({ 'left': '-300px' }, 'fast', function () {
-        $(mobilebackdrop).fadeOut('fast')
-        $(mobilenav).hide();
-    });   
-}
 
 // REQUESTS
 let userRequestContainer = $('.avatar-user-container').not('.overflow');
